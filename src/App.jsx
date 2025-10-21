14acfd991d0715c441306fce6976cf0f27fcd450
Arpit <arpit200960632242@gmail.com>
2025-10-21 18:53:20 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,72 @@
+import { useState } from "react";
+import "./App.css";
 
-import './App.css'
-
-function App() {
-  
-
+function SocialLink({ imgUrl, name, place, title, github, frontend, linkdin }) {
   return (
     <>
-      <div className='min-h-screen bg-Grey-900 flex items-center justify-center text-white'>
-        <div className='bg-Grey-800 rounded-lg p-10 text-center flex flex-col items-center'>
-          <img src="assets/images/avatar-jessica.jpeg" alt="user profile" className='rounded-full size-24'/>
-          <div className='text-3xl font-semibold'>Jessica Randall</div>
-          <div className='text-sm text-green-400 font-semibold'>London, United Kingdom</div>
-          <div>"Front-end developer and avid reader"</div>
+      <div className="min-h-screen bg-Grey-900 flex items-center justify-center text-white">
+        <div className="bg-Grey-800 rounded-lg p-10 text-center flex flex-col items-center">
+          <img
+            src={imgUrl}
+            alt="user profile"
+            className="rounded-full size-24"
+          />
+          <div className="text-3xl font-semibold">{name}</div>
+          <div className="text-sm text-green-400 font-semibold">{place}</div>
+          <div>{title}</div>
 
-          <div  className='w-full p-2 bg-Grey-700 rounded-md m-2 font-bold'>Github</div>
-          <div  className='w-full p-2 bg-Grey-700 rounded-md m-2 font-bold'>Frontend Mentor</div>
-          <div  className='w-full p-2 bg-Grey-700 rounded-md m-2 font-bold'>Linkdin</div>
-          <div  className='w-full p-2 bg-Grey-700 rounded-md m-2 font-bold'>Twitter</div>
-          <div  className='w-full p-2 bg-Grey-700 rounded-md m-2 font-bold'>Instagram</div>
+          <div className="w-full p-2 bg-Grey-700 rounded-md m-2 font-bold">
+            {github}
+          </div>
+          <div className="w-full p-2 bg-Grey-700 rounded-md m-2 font-bold">
+            {frontend}
+          </div>
+          <div className="w-full p-2 bg-Grey-700 rounded-md m-2 font-bold">
+            {linkdin}
+          </div>
+          <div className="w-full p-2 bg-Grey-700 rounded-md m-2 font-bold">
+            Twitter
+          </div>
+          <div className="w-full p-2 bg-Grey-700 rounded-md m-2 font-bold">
+            Instagram
+          </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+function App() {
+  const [hover, setHover] = useState(false);
+
+  return (
+    <div
+    onMouseEnter={()=>setHover(!hover)}
+    onMouseLeave={()=>setHover(!hover)}
+    >
+      {!hover ? (
+        <SocialLink
+          imgUrl={"assets/images/avatar-jessica.jpeg"}
+          name={"Jessica Randall"}
+          place={"London, United Kingdom"}
+          title={"Front-end developer and avid reader"}
+          github={"Github"}
+          linkdin={"Linkdin"}
+          frontend={"Frontend Mentor"}
+        />
+      ) : (
+        <SocialLink
+          imgUrl={"assets/images/personal.jpg"}
+          name={"Devank Singh"}
+          place={"Uttar Pradesh, India"}
+          title={"Front-end developer and game dev"}
+          github={<a href="https://github.com/AdreshSingh">Github</a>}
+          linkdin={<a href="linkedin.com/in/devank-singh-101k">Linkdin</a>}
+          frontend={<a href="https://www.frontendmentor.io/profile/Arpit-devank">Frontend Mentor</a>}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
